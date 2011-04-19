4097c892d5fdcd50ea3eb0c22261cdc324fa0710
magu <magu@KK-Machine.local>
2011-04-19 13:49:17 +0200

    web.xml

--- src/java/hwinventory/domain/UserApplication.java
+++ src/java/hwinventory/domain/UserApplication.java
@@ -1,11 +1,5 @@
 package hwinventory.domain;
 
-/**
-Each Wicket application is defined by an Application object. 
-This object defines what the home page is, and allows for some configuration.
-package com.helloworld.
-**/
-
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class UserApplication extends WebApplication {
--- src/java/hwinventory/domain/UserPage.java
+++ src/java/hwinventory/domain/UserPage.java
@@ -7,9 +7,33 @@ import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 
+
 public class UserPage extends WebPage {
 	
     public UserPage() {
-        
+    	User user = new User();
+    	CompoundPropertyModel userModel = new CompoundPropertyModel(user);
+    	Form form = new UserForm("user",userModel);
+    	
+    	add(form);
+    	
+    	TextField userName = new TextField("nameUser");
+    	
+    	form.add(userName);
+    	
     }
+    
+    class UserForm extends Form {
+    	
+    	public UserForm (String id, CompoundPropertyModel model) {
+    		super(id,model);
+    	}
+    	
+    	@Override
+    	
+    	public void onSubmit() {
+    	/* Print the contents of its own model object */
+    	System.out.println(getModelObject());
+    		}
+    	}   
 }
