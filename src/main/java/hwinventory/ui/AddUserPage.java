34f14fd8f076e28d5ba3d10c42af545aff363298
Jorge Ressia <ressia@jorge-ressias-macbook-pro-2.local>
2011-04-19 18:51:34 +0200

    added the add user submit functionality

--- src/main/java/hwinventory/ui/AddUserPage.java
+++ src/main/java/hwinventory/ui/AddUserPage.java
@@ -1,5 +1,6 @@
 package hwinventory.ui;
 
+import hwinventory.domain.InventoryAccess;
 import hwinventory.domain.User;
 
 import org.apache.wicket.markup.html.WebPage;
@@ -8,16 +9,31 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 
 public class AddUserPage extends WebPage {
 	
     public AddUserPage() {
     	UserDraft aUserDraft = new UserDraft();
     	CompoundPropertyModel aUserDraftModel = new CompoundPropertyModel(aUserDraft);
-    	Form form = new Form("form");
+    	Form form = new AddUserForm("form",aUserDraftModel);
     	add(form);
     	TextField userName = new TextField("name");
     	form.add(userName);
     }
+    
+    class AddUserForm extends Form {
+    	// PropertyModel is an IModel implementation 
+    	public AddUserForm (String id,IModel model) {
+    		super(id,model);
+    	}
+    	
+    	@Override 
+    	public void onSubmit() {
+    		UserDraft aUserDraftModel = (UserDraft)getModelObject();
+    		InventoryAccess anInventoryAccess = new InventoryAccess();
+    		anInventoryAccess.verifiedAnUser(aUserDraftModel.getName());
+    	}
+	}
     
 }
